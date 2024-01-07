b557f432257f0a0be1bf83e318577c346046cebd
Jakub Sulej <jakub.sulej@oroox.com>
2024-01-07 13:05:45 +0100

    Added unit test for price update

--- MarleneCollectionXmlTool.Domain.Tests/Services/ProductPriceServiceTests.cs
+++ MarleneCollectionXmlTool.Domain.Tests/Services/ProductPriceServiceTests.cs
@@ -12,12 +12,10 @@ namespace MarleneCollectionXmlTool.Domain.Tests.Services;
 public class ProductPriceServiceTests
 {
     private readonly WoocommerceDbContext _dbContext;
-    private readonly ProductPriceService _sut;
 
     public ProductPriceServiceTests()
     {
         _dbContext = FakeDbContextFactory.CreateMockDbContext<WoocommerceDbContext>();
-        _sut = new ProductPriceService(_dbContext, new ProductPromoPriceValueProvider(), A.Fake<IProductCategoryService>());
     }
 
     [Theory]
@@ -41,8 +39,10 @@ public class ProductPriceServiceTests
         await _dbContext.AddRangeAsync(metaLookups);
         await _dbContext.SaveChangesAsync();
 
+        var productPriceService = new ProductPriceService(_dbContext, new ProductPromoPriceValueProvider(), A.Fake<IProductCategoryService>());
+
         //Act
-        var result = await _sut.UpdateProductPrices(parentProducts, variantProducts, productMetaDetails, xmlProducts);
+        var result = await productPriceService.UpdateProductPrices(parentProducts, variantProducts, productMetaDetails, xmlProducts);
         await _dbContext.SaveChangesAsync();
 
         //Assert
@@ -99,8 +99,10 @@ public class ProductPriceServiceTests
         await _dbContext.AddRangeAsync(metaLookups);
         await _dbContext.SaveChangesAsync();
 
+        var productPriceService = new ProductPriceService(_dbContext, new ProductPromoPriceValueProvider(), A.Fake<IProductCategoryService>());
+
         //Act
-        var result = await _sut.UpdateProductPrices(parentProducts, variantProducts, productMetaDetails, xmlProducts);
+        var result = await productPriceService.UpdateProductPrices(parentProducts, variantProducts, productMetaDetails, xmlProducts);
         await _dbContext.SaveChangesAsync();
 
         //Assert
@@ -133,6 +135,84 @@ public class ProductPriceServiceTests
         Assert.True(regularPriceValues.All(x => x.Value == 199.00m));
         Assert.True(minPriceLookup.All(x => x.Value == 199.00m));
         Assert.True(maxPriceLookup.All(x => x.Value == 199.00m));
+    }
+
+    [Theory]
+    [InlineData("84.50", "199.00", "84.50", 1, 0)]
+    [InlineData("84.50", "199.00", "84.50", 1.2, 0)]
+    [InlineData("84.50", "199.00", "84.50", 1, 14)]
+    [InlineData("84.50", "199.00", "84.50", 1.2, 14)]
+    public async Task SynchProductPrice_ShouldSyncPriceAndAddAdditionalMargins_GivenAdditionalMarginValues(
+        string currentPrice, string currentRegularPrice, string currentSalesPrice, decimal factorMargin, decimal staticMargin)
+    {
+        //Arrange
+        _ = decimal.TryParse(currentPrice, out var currentPriceDecimal);
+        _ = decimal.TryParse(currentRegularPrice, out var currentRegularPriceDecimal);
+        _ = decimal.TryParse(currentSalesPrice, out var currentSalesPriceDecimal);
+
+        var xmlDocument = XmlTestHelper.GetXmlDocumentFromStaticFile("D20-ZIELON-Promo");
+        var xmlProducts = xmlDocument.GetElementsByTagName(HurtIvonXmlConstans.Produkt);
+
+        var originalWpPostsWithMetas = CreateMockDatabaseObjects(currentPrice, currentRegularPrice, currentSalesPrice);
+        var parentProducts = originalWpPostsWithMetas.Where(x => x.WpPost.PostType == WpPostConstans.Product).Select(x => x.WpPost).ToList();
+        var variantProducts = originalWpPostsWithMetas.Where(x => x.WpPost.PostType == WpPostConstans.ProductVariation).Select(x => x.WpPost).ToList();
+        var productMetaDetails = originalWpPostsWithMetas.SelectMany(x => x.WpPostmetum).ToList();
+        var metaLookups = originalWpPostsWithMetas.Select(x => x.ProductMetaLookup).ToList();
+
+        await _dbContext.AddRangeAsync(parentProducts);
+        await _dbContext.AddRangeAsync(variantProducts);
+        await _dbContext.AddRangeAsync(productMetaDetails);
+        await _dbContext.AddRangeAsync(metaLookups);
+        await _dbContext.SaveChangesAsync();
+
+        Environment.SetEnvironmentVariable(ConfigurationKeyConstans.PriceMarginFactor, factorMargin.ToString());
+        Environment.SetEnvironmentVariable(ConfigurationKeyConstans.PriceMarginStatic, staticMargin.ToString());
+
+        var productPriceService = new ProductPriceService(_dbContext, new ProductPromoPriceValueProvider(), A.Fake<IProductCategoryService>());
+
+        //Act
+        var result = await productPriceService.UpdateProductPrices(parentProducts, variantProducts, productMetaDetails, xmlProducts);
+        await _dbContext.SaveChangesAsync();
+
+        //Assert
+        Assert.Equal(originalWpPostsWithMetas.Count, result);
+
+        var priceValues = await _dbContext.WpPostmeta
+            .Where(x => variantProducts.Select(x => x.Id).Contains(x.PostId))
+            .Where(x => x.MetaKey == MetaKeyConstans.Price)
+            .ToDictionaryAsync(x => x.PostId, x => decimal.Parse(x.MetaValue));
+
+        var regularPriceValues = await _dbContext.WpPostmeta
+            .Where(x => variantProducts.Select(x => x.Id).Contains(x.PostId))
+            .Where(x => x.MetaKey == MetaKeyConstans.RegularPrice)
+            .ToDictionaryAsync(x => x.PostId, x => decimal.Parse(x.MetaValue));
+
+        var salePriceValues = await _dbContext.WpPostmeta
+            .Where(x => variantProducts.Select(x => x.Id).Contains(x.PostId))
+            .Where(x => x.MetaKey == MetaKeyConstans.SalePrice)
+            .ToDictionaryAsync(x => x.PostId, x => decimal.Parse(x.MetaValue));
+
+        var priceValue = (currentPriceDecimal * factorMargin) + staticMargin;
+        var salesPriceValue = (currentPriceDecimal * factorMargin) + staticMargin;
+        var regularPriceValue = (currentRegularPriceDecimal * factorMargin) + staticMargin;
+
+        Assert.True(priceValues.All(x => x.Value == priceValue));
+        Assert.True(salePriceValues.All(x => x.Value == salesPriceValue));
+        Assert.True(regularPriceValues.All(x => x.Value == regularPriceValue));
+
+        var minPriceLookup = await _dbContext.WpWcProductMetaLookups
+            .Where(x => variantProducts.Select(x => (long)x.Id).Contains(x.ProductId))
+            .ToDictionaryAsync(x => x.ProductId, x => x.MinPrice);
+
+        var maxPriceLookup = await _dbContext.WpWcProductMetaLookups
+            .Where(x => variantProducts.Select(x => (long)x.Id).Contains(x.ProductId))
+            .ToDictionaryAsync(x => x.ProductId, x => x.MaxPrice);
+
+        Assert.True(minPriceLookup.All(x => x.Value == currentPriceDecimal));
+        Assert.True(maxPriceLookup.All(x => x.Value == regularPriceValue));
+
+        Environment.SetEnvironmentVariable(ConfigurationKeyConstans.PriceMarginFactor, null);
+        Environment.SetEnvironmentVariable(ConfigurationKeyConstans.PriceMarginStatic, null);
     }
 
     private static List<MockDataHelper.WpPostWithMeta> CreateMockDatabaseObjects(string price, string regularPrice, string salesPrice)
