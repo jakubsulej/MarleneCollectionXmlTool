202c9606ba477e1cda42de7e20a5588b7dd18d2e
Jakub Sulej <jakub.sulej@oroox.com>
2024-01-07 11:01:33 +0100

    Added unit test for env variable helper method

---
+++ MarleneCollectionXmlTool.Domain.Tests/Helpers/EnvironmentVariableHelpersTests.cs
@@ -0,0 +1,39 @@
+﻿using MarleneCollectionXmlTool.Domain.Helpers;
+using Xunit;
+
+namespace MarleneCollectionXmlTool.Domain.Tests.Helpers;
+
+public class EnvironmentVariableHelpersTests
+{
+    [Theory]
+    [InlineData(23)]
+    [InlineData(2.1)]
+    public void GetEnvironmentVariableOrDefault_ShouldReturnParsedValue_GivenDecimalValue(decimal value)
+    {
+        //Arrange
+        var environementName = "EnvironmentVariableName";
+        Environment.SetEnvironmentVariable(environementName, value.ToString());
+
+        //Act
+        var result = EnvironmentVariableHelpers.GetEnvironmentVariableOrDefault<decimal>(environementName);
+
+        //Assert
+        Assert.Equal(value, result);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void GetEnvironmentVariableOrDefault_ShouldReturnParsedValue_GivenBoolValue(bool value)
+    {
+        //Arrange
+        var environementName = "EnvironmentVariableName";
+        Environment.SetEnvironmentVariable(environementName, value.ToString());
+
+        //Act
+        var result = EnvironmentVariableHelpers.GetEnvironmentVariableOrDefault<bool>(environementName);
+
+        //Assert
+        Assert.Equal(value, result);
+    }
+}
